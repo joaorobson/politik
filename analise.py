29ebc75f127c110ccffb91b4c06e34a40d36f2ef
joaorobson <joaorobsonmartins@gmail.com>
2018-11-27 16:41:45 -0200

    Solve reading bug

--- analise.py
+++ analise.py
@@ -3,6 +3,7 @@ import string
 from wordcloud import WordCloud
 from nltk.corpus import stopwords
 from collections import Counter
+import unidecode
 
 
 preposicoes = ['a', 'ante', 'ate', 'apos','com', 'contra', 'de', 'desde',
@@ -13,7 +14,7 @@ combinacoes = ["ao", "aos", "as", "do", "dos", "da", "das", "dum", "duns", "duma
 
 artigos = ['a', 'o', 'as', 'os', 'um', 'uns', 'uma', 'umas','e', 'que']
 
-nomes = ['bolsonaro2018', 'haddad', 'luiz', 'inacio', 'lula', 'silva', 'mais', 'se', 'acima']
+nomes = ['bolsonaro2018', 'haddad', 'luiz', 'inacio', 'lula', 'silva', 'mais', 'se', 'acima', 'r$']
 
 nao_substantivos = preposicoes + artigos + nomes + combinacoes + stopwords.words('portuguese')
 def remove_accents(s):
@@ -21,11 +22,18 @@ def remove_accents(s):
 
 def read_file(f):
     text = f.read()
+    bla = [i for i in text.split() if i.lower() not in nao_substantivos and len(i) > 1]
+    print(bla.count('R$'))
+    text = ' '.join(bla)
     text = remove_accents(text)
-    bla = (text.split())
-    bla = [i for i in text.split() if i not in nao_substantivos]
-    response = [{'word':i, 'value':bla.count(i)} for i in set(bla)]
-    print(f.name)
+    j = 0
+    for i in text.split():
+        if i == 'r':
+            print(text.split()[j-1], text.split()[j+1])
+        j += 1
+    #print(sorted(set(text.split())))
+    response = [{'word':i, 'value':bla.count(i)} for i in set(bla) if bla.count(i) > 1]
+    #print(f.name)
     a = open(f.name + str(1) ,'w')
     a.write(str(response))
     a.close()
@@ -33,6 +41,7 @@ def read_file(f):
     return text
 
 if __name__ == '__main__':
+    '''
     f = open('bolsonaro.txt','r')
     text1 = (read_file(f))
     f.close()
@@ -42,19 +51,26 @@ if __name__ == '__main__':
     f3 = open('ciro.txt','r')
     text3 = (read_file(f3))
     f3.close()
+    '''
     f4 = open('amoedo.txt','r')
     text4 = (read_file(f4))
     f4.close()
+    '''
     f5 = open('alckmin.txt','r')
     text5 = (read_file(f5))
     f5.close()
+    '''
+    '''
     wordcloud1 = WordCloud().generate(text1)
     wordcloud2 = WordCloud().generate(text2)
     wordcloud3 = WordCloud().generate(text3)
+    '''
     wordcloud4 = WordCloud().generate(text4)
-    wordcloud5 = WordCloud().generate(text5)
+    
+    #wordcloud5 = WordCloud().generate(text5)
     #a = text.split()
     import matplotlib.pyplot as plt
+    '''
     plt.figure(1)
     plt.title('bolsonaro')
     plt.imshow(wordcloud1, interpolation='bilinear')
@@ -77,19 +93,19 @@ if __name__ == '__main__':
     wordcloud2 = WordCloud(max_font_size=40).generate(text3)
     plt.imshow(wordcloud3, interpolation='bilinear')
     plt.axis("off")
-
+    '''
     plt.figure(4)
     plt.imshow(wordcloud4, interpolation='bilinear')
     plt.axis("off")
     wordcloud2 = WordCloud(max_font_size=40).generate(text4)
     plt.imshow(wordcloud4, interpolation='bilinear')
     plt.axis("off")
-
+    '''
     plt.figure(5)
     plt.imshow(wordcloud5, interpolation='bilinear')
     plt.axis("off")
     wordcloud2 = WordCloud(max_font_size=40).generate(text5)
     plt.imshow(wordcloud5, interpolation='bilinear')
     plt.axis("off")
-
+    '''
     plt.show()
